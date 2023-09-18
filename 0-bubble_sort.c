d6d4691d3d4da5a9a4c8ffd47c9e911e2e23866f
maouia <maouiabend@gmail.com>
2023-09-18 20:26:38 +0100

    0-bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+/**
+ * swap -sawp a and b
+ * @a: intiger
+ * @b: intiger
+*/
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
+ * bubble_sort - bubble sorting
+ * @array: array t sort it
+ * @size: size of array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	if (!array || size == 0)
+		return;
+	size_t i, j;
+	int swaped;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swaped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				swaped = 1;
+				print_array(array, size);
+			}
+		}
+		if (swaped == 0)
+		{
+			return;
+		}
+	}
+}
